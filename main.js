62de9139d0611343ed790f7a6b483a6ec62d7817
Jessica Felts <thejessicafelts@gmail.com>
2018-09-23 22:41:06 -0400

    updated program to add unique ids to all new rows.

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
+const accounts = document.getElementById("accounts");
+let rowCount = accounts.rows.length;
+
 document.getElementById("newRowBtn").addEventListener("click", newRow);
 
 function newRow(){
-  let accounts = document.getElementById("accounts");
-  let row = accounts.insertRow();
-  let newAccount = row.insertCell(0);
-  let newBalance = row.insertCell(1);
-  let newInterestRate = row.insertCell(2);
-  let newPayment = row.insertCell(3);
-  let newDelete = row.insertCell(4);
+  let newRow = accounts.insertRow(-1);
+  let newAccount = newRow.insertCell(0);
+  let newBalance = newRow.insertCell(1);
+  let newInterestRate = newRow.insertCell(2);
+  let newPayment = newRow.insertCell(3);
   newAccount.innerHTML = "<input type='text'>";
   newBalance.innerHTML = "<input type='number' min='0.01' step='0.01'>";
   newInterestRate.innerHTML = "<input type='number' min='0.01' step='0.01'>";
   newPayment.innerHTML = "<input type='number' min='0.01' step='0.01'>";
-  newDelete.innerHTML = "<span id='deleteRowBtn'><i class='fa fa-minus-circle'></i></span>";
-  newDelete.className = "delete";
+  rowCount++;
+  newRow.id = 'row-' + rowCount;
+}
+
+document.getElementById("deleteRowBtn").addEventListener("click", deleteRow);
+
+function deleteRow(){
+  let accounts = document.getElementById("accounts");
+  let rowCount = accounts.rows.length;
+  accounts.deleteRow(rowCount -1);
 }
