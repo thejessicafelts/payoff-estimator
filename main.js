ac1ed8139c23f0452f05726e2e499694fbbe7007
Jessica Felts <thejessicafelts@gmail.com>
2018-10-03 21:52:28 -0400

    updated project files.

--- main.js
+++ main.js
@@ -9,18 +9,23 @@ function newRow(){
   let newBalance = newRow.insertCell(1);
   let newInterestRate = newRow.insertCell(2);
   let newPayment = newRow.insertCell(3);
+  let newDelete = newRow.insertCell(4);
   newAccount.innerHTML = "<input type='text'>";
   newBalance.innerHTML = "<input type='number' min='0.01' step='0.01'>";
   newInterestRate.innerHTML = "<input type='number' min='0.01' step='0.01'>";
   newPayment.innerHTML = "<input type='number' min='0.01' step='0.01'>";
   rowCount++;
   newRow.id = 'row-' + rowCount;
+  newDelete.innerHTML = "<input class='delete' type='button' value='Delete Row'>";
 }
 
-document.getElementById("deleteRowBtn").addEventListener("click", deleteRow);
+let deleteBtn = document.getElementsByClassName("deleteRowBtn");
 
-function deleteRow(){
-  let accounts = document.getElementById("accounts");
-  let rowCount = accounts.rows.length;
-  accounts.deleteRow(rowCount -1);
-}
+
+
+$('input[type="button"].delete').click(function(e){
+  let currentRow = $(this).parent();
+  currentRow.remove();
+  alert('success!');
+});
+$('input[type="button"].delete').off('click');
